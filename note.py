14e9ec61d0f3165d6eb79c7e2f636c0bbcd4b8ba
Samad Mirzayev <samed.mirzayev@outlook.com>
2020-06-25 13:04:00 +0400

    Closes:#2

---
+++ note.py
@@ -0,0 +1,20 @@
+def save_note(name: str, content: str, folder: str) -> bool:
+	with open(f"{folder}/{name}.txt", "w+") as file:
+		file.write(content)
+	return True
+
+def get_note(name: str) -> str:
+	with open(f"{NOTES_DIR}/{name}.txt", "r") as file:
+		data = file.read()
+	return data
+
+
+def build_note(rows=5) -> str:
+	i = rows
+	lines = []
+	while i>0:
+		line = input("Add a note:\t")
+		lines.append(line)
+		i -= 1
+
+	return "\n".join(lines)
